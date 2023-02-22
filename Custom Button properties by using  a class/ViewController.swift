43827e60f43c3d49bf1a738019d59050b68f3c42
Carrington <Manyuchi>
2023-02-22 22:41:46 +0200

    Initial Commit

---
+++ Custom Button properties by using  a class/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Button properties by using  a class
+//
+//  Created by Carrington Tafadzwa Manyuchi on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
